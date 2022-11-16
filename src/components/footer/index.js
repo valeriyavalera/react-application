6ac3b7b887eff63e2b3a29af4afe443c6363d3ff
Mona Lisa <leramutyk1@gmail.com>
2022-11-16 16:33:36 +0200

    message

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -50,7 +50,13 @@ function Footer() {
       </div>
       <div className="contact">
         <h3>Contact Us</h3>
-        
+        <label for="smaller1">Name *</label><br></br>
+        <input id="smaller1" type="text" ></input><br></br>
+        <label for="smaller2">Email *</label><br></br>
+        <input id="smaller2" type="text"></input><br></br>
+        <input id="bigger" type="text"></input><br></br>
+        <button className="contact_btn_orange">SUBMIT</button>
+        <button className="contact_btn_gray">RESET</button>
         
 
       </div>
